e66f220bb9f5a2879abb7e8e5f2d5a85457f1161
kimminh2903 <kimminh201204@gmail.com>
2024-05-15 14:00:52 +0700

    php

---
+++ chuyenkhoa.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/chuyenkhoa.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.min.js"></script>
+    <title>Chuyên Khoa</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="chidan">
+                <a href></a>
+            </div>
+        </div>
+        <div class="row">
+        
+            <div class="col-lg-3 khungck">
+                <a href="inforCK.php">
+                <div class="imgck">
+                    <img src="img/xuongkhop.jpg" alt="">
+                </div>
+                <div class="tenck">
+                    <p>Khoa Xương Khớp</p>
+                </div>
+                </a>
+            </div>
+            <div class="col-lg-3 khungck">
+                <div class="imgck">
+                    <img src="img/thankinh.jpg" alt="">
+                </div>
+                <div class="tenck">
+                    <p>Khoa Thần Kinh</p>
+                </div>
+            </div>
+            <div class="col-lg-3 khungck">
+                <div class="imgck">
+                    <img src="img/tieuhoa.jpg" alt="">
+                </div>
+                <div class="tenck">
+                    <p>Khoa Tiêu Hóa</p>
+                </div>
+            </div>
+            <div class="col-lg-3 khungck">
+                <div class="imgck">
+                    <img src="img/timmach.jpg" alt="">
+                </div>
+                <div class="tenck">
+                    <p>Khoa Xương Tim Mạch</p>
+                </div>
+            </div>
+            
+        </div>
+        <div class="row">
+            <div class="col-lg-3 khungck">
+                    <div class="imgck">
+                        <img src="img/taimuihong.jpg" alt="">
+                    </div>
+                    <div class="tenck">
+                        <p>Khoa Xương Tai Mũi Họng</p>
+                    </div>
+            </div>
+            <div class="col-lg-3 khungck">
+                    <div class="imgck">
+                        <img src="img/cotsong.jpg" alt="">
+                    </div>
+                    <div class="tenck">
+                        <p>Khoa Cột Sống </p>
+                    </div>
+            </div>
+            <div class="col-lg-3 khungck">
+                    <div class="imgck">
+                        <img src="img/yhoccotruyen.jpg" alt="">
+                    </div>
+                    <div class="tenck">
+                        <p>Khoa Y Học Cổ Truyền</p>
+                    </div>
+            </div>
+            <div class="col-lg-3 khungck">
+                    <div class="imgck">
+                        <img src="img/chamcuu.jpg" alt="">
+                    </div>
+                    <div class="tenck">
+                        <p>Khoa Châm Cứu </p>
+                    </div>
+            </div>
+
+        </div>
+        <div class="row">
+            <div class="col-lg-3 khungck">
+                    <div class="imgck">
+                        <img src="img/sanphukhoa.jpg" alt="">
+                    </div>
+                    <div class="tenck">
+                        <p>Sản Phụ Khoa</p>
+                    </div>
+            </div>
+            <div class="col-lg-3 khungck">
+                    <div class="imgck">
+                        <img src="img/sieuamthai.jpg" alt="">
+                    </div>
+                    <div class="tenck">
+                        <p>Siêu Âm Thai</p>
+                    </div>
+            </div>
+            <div class="col-lg-3 khungck">
+                    <div class="imgck">
+                        <img src="img/nhikhoa.jpg" alt="">
+                    </div>
+                    <div class="tenck">
+                        <p>Nhi Khoa</p>
+                    </div>
+            </div>
+            <div class="col-lg-3 khungck">
+                    <div class="imgck">
+                        <img src="img/dalieu.jpg" alt="">
+                    </div>
+                    <div class="tenck">
+                        <p>Da Liễu</p>
+                    </div>
+            </div>
+
+        </div>
+        <div class="row">
+            <div class="col-lg-3 khungck">
+                    <div class="imgck">
+                        <img src="img/benhviemgan.jpg" alt="">
+                    </div>
+                    <div class="tenck">
+                        <p>Bệnh Viêm Gan</p>
+                    </div>
+            </div>
+            <div class="col-lg-3 khungck">
+                    <div class="imgck">
+                        <img src="img/sktamthan.jpg" alt="">
+                    </div>
+                    <div class="tenck">
+                        <p>Sức Khỏe Tâm Thần</p>
+                    </div>
+            </div>
+            <div class="col-lg-3 khungck">
+                    <div class="imgck">
+                        <img src="img/diungmiendich.jpg" alt="">
+                    </div>
+                    <div class="tenck">
+                        <p>Dị Ứng Miễn Dịch</p>
+                    </div>
+            </div>
+            <div class="col-lg-3 khungck">
+                    <div class="imgck">
+                        <img src="img/hohapphoi.jpg" alt="">
+                    </div>
+                    <div class="tenck">
+                        <p>Hô Hấp - Phổi</p>
+                    </div>
+            </div>
+
+        </div>
+        
+    </div>
+    
+</body>
+</html>
---
+++ inforCK.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/inforCK.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.min.js"></script>
+    <title>Khoa Xương Khớp</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="chidan">
+                <a href></a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <h4>Cơ Xương Khớp</h4>
+                <h5>Bác sĩ cơ xương khớp giỏi</h5>
+                <p>Danh sách các bác sĩ uy tín đầu ngành Cơ Xương Khớp tại Việt Nam:</p>
+                        <ul>
+                            <li>Các chuyên gia có quá trình đào tạo bài bản, nhiều kinh nghiệm</li>
+                            <li>Các giáo sư, phó giáo sư đang trực tiếp nghiên cứu và giảng dạy tại Đại học Y khoa Hà Nội</li>
+                            <li>Các bác sĩ đã, đang công tác tại các bệnh viện hàng đầu Khoa Cơ Xương Khớp - Bệnh viện Bạch Mai, Bệnh viện Hữu nghị Việt Đức,Bệnh Viện E</li>
+                            <li>Là thành viên hoặc lãnh đạo các tổ chức chuyên môn như: Hiệp hội Cơ Xương Khớp, Hội Thấp khớp học,...</li>
+                            <li>Được nhà nước công nhận các danh hiệu Thầy thuốc Nhân dân, Thầy thuốc Ưu tú, Bác sĩ Cao cấp,...</li>
+                        </ul>
+                        <h5>Bệnh Cơ Xương Khớp</h5>
+                        <ul>
+                            <li>Gout</li>
+                            <li>Thoái hóa khớp: khớp gối, cột sống thắt lưng, cột sống cổ</li>
+                            <li>Viêm khớp dạng thấp, Viêm đa khớp, Viêm gân</li>
+                            <li>Loãng xương, đau nhức xương</li>
+                            <li>Viêm xương, gai xương</li>
+                            <li>Viêm cơ, Teo cơ, chứng đau mỏi cơ</li>
+                            <li>Yếu cơ, Loạn dưỡng cơ</li>
+                            <li>Các chấn thương về cơ, xương, khớp</li>
+                            <li>...</li>
+                        </ul>                       
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <select name="" id="Khuvuc">
+                    <option value="">Toàn Quốc</option>
+                    <option value="">Hà Nội</option>
+                    <option value="">Tp.Hồ Chí Minh</option>
+                </select>
+            </div>
+        </div>
+        <div class="row khung">
+            <div class="col-lg-6">
+                <div class="row">
+                    <div class="col-sm-3">
+                        <div class="imgdoctor">
+                            <img src="img/bacsi.jpg" alt="">
+                        </div>
+                        <a href="">Xem thêm</a>
+                    </div>
+                    <div class="col-sm-9">
+                        <h5><a href="">Bác sĩ Chuyên Khoa II Trần Trọng Thắng</a></h5>
+                        <p>
+                            <ul>
+                                <li>Gần 30 năm kinh nghiệm lĩnh vực Cơ xương khớp</li>
+                                <li>Từng công tác tại Bệnh viện Xanh Pôn, Bệnh viện Phục hồi chức năng Hà Nội</li>
+                                <li>Bác sĩ nhận khám tất cả các độ tuổi</li>
+                            </ul>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div>
+                    <label for="date">Chọn Ngày:</label>
+                    <input type="date" id="date" name="date">
+                </div>
+                <h6>LỊCH KHÁM</h6>
+                <div class="row">
+                    <div class="col-sm-3">
+                        <button>07:30-08:00</button>
+                    </div>
+                    <div class="col-sm-3">
+                        <button>07:30-08:00</button>
+                    </div>
+                    <div class="col-sm-3">
+                        <button>07:30-08:00</button>
+                    </div>
+                    <div class="col-sm-3">
+                        <button>07:30-08:00</button>
+                    </div>
+                    <div class="col-sm-3">
+                        <button>07:30-08:00</button>
+                    </div>
+                    <div class="col-sm-3">
+                        <button>07:30-08:00</button>
+                    </div>
+                    <div class="col-sm-3">
+                        <button>07:30-08:00</button>
+                    </div>
+                    <div class="col-sm-3">
+                        <button>07:30-08:00</button>
+                    </div>
+                    <div class="col-sm-3">
+                        <button>07:30-08:00</button>
+                    </div>
+                    <div class="col-sm-3">
+                        <button>07:30-08:00</button>
+                    </div>
+                    <div class="col-sm-3">
+                        <button>07:30-08:00</button>
+                    </div>
+                    <div class="col-sm-3">
+                        <button>07:30-08:00</button>
+                    </div>
+                    
+                </div>
+                <h6>ĐỊA CHỈ KHÁM</h6>
+                <p>3 Đường Số 17A, Bình Trị Đông B, Bình Tân, Thành phố Hồ Chí Minh</p>
+                <h6>GIÁ KHÁM: 150000đ</h6>
+                <h6>LOẠI BẢO HIỂM ÁP DỤNG.</h6>(LÀM MODAL HIỂN THỊ BẢNG BẢO HIỂM)
+
+
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
